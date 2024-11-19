ecd2d7c7f39f4e315eff1a99b6007cd4ca2b21dd
AmosBunde <git.bunde@hotmail.com>
2024-11-19 22:21:48 +0300

    Creating the Dockerfile and docker-compose for the app

---
+++ cr8s/target/debug/deps/cr8s-aee7dd67aa5f08dd.d
@@ -0,0 +1,5 @@
+/home/architect/Documents/RustAPI/cr8s/target/debug/deps/libcr8s-aee7dd67aa5f08dd.rmeta: src/main.rs
+
+/home/architect/Documents/RustAPI/cr8s/target/debug/deps/cr8s-aee7dd67aa5f08dd.d: src/main.rs
+
+src/main.rs:
---
+++ cr8s/target/debug/deps/cr8s-d40f8b6e56b749e1.d
@@ -0,0 +1,5 @@
+/home/architect/Documents/RustAPI/cr8s/target/debug/deps/libcr8s-d40f8b6e56b749e1.rmeta: src/main.rs
+
+/home/architect/Documents/RustAPI/cr8s/target/debug/deps/cr8s-d40f8b6e56b749e1.d: src/main.rs
+
+src/main.rs:
